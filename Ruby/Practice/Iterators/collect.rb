3374abb2664702e115e42118dd056ee597281af1
aarzoo <aarzoo@berylsystems.com>
2024-03-28 18:33:11 +0530

    adding data related to iterators in ruby notes
    and subsequent practice files in Practice/Iterators

---
+++ Ruby/Practice/Iterators/collect.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+digits = [1,2,3,4,5]
+
+a = digits.collect{|x|10*x}
+
+puts "#{a}"
+
+
+#output - 
+#       [10, 20, 30, 40, 50]
---
+++ Ruby/Practice/Iterators/each.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+digits = 0..5
+
+digits.each do |digits|
+    puts "In loop #{digits}"
+end
+
+#output - 
+#       In loop 0
+#       In loop 1
+#       In loop 2
+#       In loop 3
+#       In loop 4
+#       In loop 5
