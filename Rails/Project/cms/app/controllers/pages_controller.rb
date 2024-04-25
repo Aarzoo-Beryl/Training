703e08ef53fa5f53107653c53d105f8aaff05dce
Beryl <aarzoo@berylsystems.com>
2024-04-25 18:07:47 +0530

    adds index to cms for pages

--- Rails/Project/cms/app/controllers/pages_controller.rb
+++ Rails/Project/cms/app/controllers/pages_controller.rb
@@ -1,26 +1,54 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.order('position ASC')
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    # Instantiate a new object using form parameters
+    @page = Page.new(page_params)
+
+    # Save the object
+    if @page.save
+      redirect_to(pages_path)      # If save succeeds,redirect to index page
+    else
+      render('new')     # If save fails,redisplay the form so user can fix it
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to(page_path(@page))
+    else
+      render('new')
+    end
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
 
   def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to(pages_path)
   end
-  
+
+  private
+  def page_params
+    params.require(:subject).permit(:name,:position,:visible,:subject_id,:permalink,:content)
+  end
 end
