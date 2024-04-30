5b01e9ba67048e9f13a0f5a3e36357c92769fcb8
Beryl Systems <aarzoo@berylsystems.com>
2024-04-30 18:24:41 +0530

    adds html syntax for views of controller:User

--- Rails/Project/website/app/controllers/user_controller.rb
+++ Rails/Project/website/app/controllers/user_controller.rb
@@ -1,25 +1,48 @@
 class UserController < ApplicationController
   def index
+    @users = User.order("id ASC")
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+    if @user.save
+      redirect_to(users_path)
+    else
+      render('new')
+    end
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to(users_path)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+  def user_params
   end
 end
