59e4e0bd3b5b3c98199a5af5628f671a74a8f332
Beryl Systems <aarzoo@berylsystems.com>
2024-04-30 17:09:27 +0530

    adds new controller User to website

--- Rails/Project/website/config/routes.rb
+++ Rails/Project/website/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/edit'
+  get 'user/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
