5d82823666f0c7820b9c9f35105d484109ade818
Beryl Systems <aarzoo@berylsystems.com>
2024-05-02 12:19:44 +0530

    adds custom validation for email in lms

--- Rails/Project/library_management_sysytem/app/models/memeber.rb
+++ Rails/Project/library_management_sysytem/app/models/memeber.rb
@@ -1,7 +1,7 @@
 class Memeber < ApplicationRecord
   validates :name, :email, :address, :phone_number, :membership_status , presence: true
   validates :name, format: {with: /\A[a-zA-z\s]+\z/ , message: "only aphabets are allowed in name field"}
-  validates :email, format: { with: URL::MailTo::EMAIL_REGEXP, message: "kindly write correct email format"}
   validates :phone_number, numericality: {only_integer: true, message: "only integers are allowed"}, length: { is: 10, message: "length of phone number must be 10"}
   validates :membership_status, inclusion: {in: %w(active inactive), message: "please define correct value for membership_status"}
+  validates_with EmailValidator
 end
