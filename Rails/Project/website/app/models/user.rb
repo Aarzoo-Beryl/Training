f8bc4382bf6381de7bb65deb4f8a4ca62e18001d
Beryl Systems <aarzoo@berylsystems.com>
2024-05-02 12:22:08 +0530

    adds custom validation for email in website

--- Rails/Project/website/app/models/user.rb
+++ Rails/Project/website/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, :email, :phone_number, :gender, :address, presence: true
   validates :name , format: { with: /\A[a-zA-Z\s]+\z/, message: "only alphabets are allowed" }
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "format must be of type: abc@example.com" }
+  validates :email, uniqueness: true, email: true
   validates :phone_number, uniqueness: true, length: {is: 10, message:"must be 10 digits"}, numericality: { only_integer: true }
   validates :gender, inclusion: { in:%w(Male Female Other (Male Female Other).map(&:downcase)), message: "%{value} is not a valid gender, Please specify something among (Male Female Other)" }
 
