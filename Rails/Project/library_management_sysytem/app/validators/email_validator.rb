942ea995757a605f37bab161dac37c114565b2ee
Beryl Systems <aarzoo@berylsystems.com>
2024-05-02 19:03:08 +0530

    adds file association to model_layer
    this file contains notes on association topic

--- Rails/Project/library_management_sysytem/app/validators/email_validator.rb
+++ Rails/Project/library_management_sysytem/app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::Validator
   def email_validation(record)
-    unless record.email == URL::MailTo::EMAIL_REGEXP
+    unless record.email =~ URL::MailTo::EMAIL_REGEXP
       record.errors.add(:email,message: 'format not correct')
     end
   end
