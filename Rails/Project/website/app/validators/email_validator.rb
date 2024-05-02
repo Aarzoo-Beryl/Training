0fd0cb9150a78f6642ebd502e8021aeda8566d7a
Beryl Systems <aarzoo@berylsystems.com>
2024-05-02 12:25:48 +0530

    corrects custom validation for email

--- Rails/Project/website/app/validators/email_validator.rb
+++ Rails/Project/website/app/validators/email_validator.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::Validator
-  def email_validation(record)
-    unless record.email == URL::MailTo::EMAIL_REGEXP
-      record.errors.add(:email,message: 'format must be of type: abc@example.com')
+  def validate(record)
+    unless record.email =~ URI::MailTo::EMAIL_REGEXP
+      record.errors.add(:email, message: 'format must be of type: abc@example.com')
     end
   end
 end
