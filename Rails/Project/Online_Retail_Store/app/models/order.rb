63c13fac9a19fa01b099292bf47c4d531cab3583
Beryl Systems <aarzoo@berylsystems.com>
2024-05-14 18:19:00 +0530

    adds queries on scopes and dynamic finders

--- Rails/Project/Online_Retail_Store/app/models/order.rb
+++ Rails/Project/Online_Retail_Store/app/models/order.rb
@@ -8,15 +8,20 @@ class Order < ApplicationRecord
   validates :date, presence: true
   validates :shipping_address, presence: true
 
+  default_scope {where('customer_id > 1')}
 
-  def update_total_bill
-    self.total_bill = calculate_total_bill
+  def self.total_amount_greater_than(amount)
+    where('total_order > ?', amount) if amount.present?
+  end
+
+  def update_total_order
+    self.total_order = calculate_total_order
     save
   end
 
   private
 
-  def calculate_total_bill
+  def calculate_total_order
     products.sum(:price)
   end
 end
--- Rails/Project/Online_Retail_Store/app/models/review.rb
+++ Rails/Project/Online_Retail_Store/app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   belongs_to :customer, validate: true
 
   validates :description, presence: true
+  scope :customer, -> { where('reviews.customer_id >?',1)}
+  scope :product, -> { where('reviews.product_id >?',1)}
 end
