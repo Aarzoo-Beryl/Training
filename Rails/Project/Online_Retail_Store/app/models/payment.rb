53bd53b97bf6ee9f0b9dc3bc489e4848cc6eeef2
Beryl Systems <aarzoo@berylsystems.com>
2024-07-02 14:46:51 +0530

    correcting associations in ors project

--- Rails/Project/Online_Retail_Store/app/models/payment.rb
+++ Rails/Project/Online_Retail_Store/app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :payment_type, validate: true
   belongs_to :order, validate: true ,dependent: :destroy
-  has_one :customer, through: :order, disable_joins: true ,->(payment) { where status: "paid")}
+  has_one :customer, through: :order, disable_joins: true 
 
   validates :date, presence: true
   validates :status, presence: true
