7b44b03c0898e43aa2f215f1a8c4a7644c58d7d0
Beryl Systems <aarzoo@berylsystems.com>
2024-07-03 00:58:32 +0530

    adds callback before and after validation

--- Rails/Project/Online_Retail_Store/app/models/customer.rb
+++ Rails/Project/Online_Retail_Store/app/models/customer.rb
@@ -7,4 +7,20 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true, uniqueness: true
+
+  before_validation :check_name, on: [:create, :update]
+
+  after_validation :check_phone_number
+
+  private
+    def check_name
+      if name.blank?
+        puts "name is blank"
+      end
+    end
+
+    def check_phone_number
+      @a = self.phone_number
+      puts "ensure your phone number is #{@a} "
+    end
 end
