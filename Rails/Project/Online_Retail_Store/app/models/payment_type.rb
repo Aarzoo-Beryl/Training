72e370f4730d6d2ce5e33c99a11421b82d4aab8b
Beryl Systems <aarzoo@berylsystems.com>
2024-07-03 12:17:43 +0530

    adds after commit and after rollback callbacks [project:ors]

--- Rails/Project/Online_Retail_Store/app/models/payment_type.rb
+++ Rails/Project/Online_Retail_Store/app/models/payment_type.rb
@@ -6,6 +6,10 @@ class PaymentType < ApplicationRecord
   before_create :capitalize_title
   around_create :lifecycle_info
   after_create :confirmation_info
+
+  after_commit :commit_info
+  after_rollback :rollback_info
+
   private
      def capitalize_title
        title.capitalize! if title.present?
@@ -17,5 +21,11 @@ class PaymentType < ApplicationRecord
      end
      def confirmation_info
        puts("Payment type was successfully created")
+     end
+     def commit_info
+       puts("The transaction was successfully committed for PaymentType #{self.title}")
+     end
+     def rollback_info
+       puts("The transaction was rolled back because of : #{self.error.full_messages.join(',')}")
      end
 end
