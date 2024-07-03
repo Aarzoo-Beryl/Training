3040c762fabd3aa1c5da35fdb6a5979bb1f08c7f
Beryl Systems <aarzoo@berylsystems.com>
2024-07-03 12:28:47 +0530

    adds before,around and after update callbacks [project:ors]

--- Rails/Project/Online_Retail_Store/app/models/payment_type.rb
+++ Rails/Project/Online_Retail_Store/app/models/payment_type.rb
@@ -4,17 +4,21 @@ class PaymentType < ApplicationRecord
   validates :title, presence: true
 
   before_create :capitalize_title
-  around_create :lifecycle_info
+  around_create :lifecycle_info_create
   after_create :confirmation_info
 
   after_commit :commit_info
   after_rollback :rollback_info
 
+  before_update :capitalize_title
+  around_update :lifecycle_info_update
+  after_update :updation_info
+
   private
      def capitalize_title
        title.capitalize! if title.present?
      end
-     def lifecycle_info
+     def lifecycle_info_create
          puts("around create : before create")
          yield
          puts("around create : after create")
@@ -27,5 +31,13 @@ class PaymentType < ApplicationRecord
      end
      def rollback_info
        puts("The transaction was rolled back because of : #{self.error.full_messages.join(',')}")
+     end
+     def lifecycle_info_update
+         puts("around update : before update")
+         yield
+         puts("around update : after update")
+     end
+     def updation_info
+       puts("The object with id:#{self.id} was successfully updated")
      end
 end
