6aa1a9f4c6067f54071c9a420a70df291f916375
Beryl Systems <aarzoo@berylsystems.com>
2024-07-03 13:00:07 +0530

    adds after initialize, after find and after touch callbacks [project:ors]

--- Rails/Project/Online_Retail_Store/app/models/brand.rb
+++ Rails/Project/Online_Retail_Store/app/models/brand.rb
@@ -2,4 +2,19 @@ class Brand < ApplicationRecord
   has_many :products, dependent: :destroy
 
   validates :title, presence: true
+
+  after_initialize :log_initialize
+  after_find :log_found
+  after_touch :log_touched
+
+  private
+    def log_initialize
+      puts("you have initialized an new object")
+    end
+    def log_found
+      puts("you have found a new object with id: #{self.id}")
+    end
+    def log_touched
+      puts("you have touched the object with id: #{self.id}")
+    end
 end
