fb6f9e1ee5132107c8bd579a5e6e60d962ace6ef
Beryl Systems <aarzoo@berylsystems.com>
2024-07-03 18:22:44 +0530

    add association callbacks

--- Rails/Project/Social_Media_Platform/app/models/user.rb
+++ Rails/Project/Social_Media_Platform/app/models/user.rb
@@ -1,15 +1,24 @@
 class User < ApplicationRecord
-  has_many :posts, inverse_of: :user,  dependent: :destroy
+  has_many :posts, inverse_of: :user, dependent: :destroy, before_add: :before_add_post, after_add: :after_add_post, before_remove: :before_remove_post, after_remove: :after_remove_post
   has_many :friendships, inverse_of: :user
   has_many :comments, as: :commentable, dependent: :destroy, inverse_of: :user
   has_many :likes, as: :likeable, dependent: :destroy, inverse_of: :user
   has_many :notifications, as: :notifiable
 
 
-  after_destroy :destroy_info
+  def before_add_post(post)
+    puts "About to add post #{post.id} to user #{self.id}"
+  end
 
-  private
-    def destroy_info
-      puts("post related to user #{self.name} is destroyed")
-    end
+  def after_add_post(post)
+    puts "Added post #{post.id} to user #{self.id}"
+  end
+
+  def before_remove_post(post)
+    puts "About to remove post #{post.id} from user #{self.id}"
+  end
+
+  def after_remove_post(post)
+    puts "Removed post #{post.id} from user #{self.id}"
+  end
 end
