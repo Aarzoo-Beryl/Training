09720fd9db28617bdb85fc89e2771b0c3af2819e
Beryl Systems <aarzoo@berylsystems.com>
2024-07-10 16:19:18 +0530

    adds edit,update,delete,destroy method to the controller

--- Rails/Project/Social_Media_Platform/app/controllers/user_controller.rb
+++ Rails/Project/Social_Media_Platform/app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
-	
+
 	def list
 		@users = User.all
 	end
@@ -16,6 +16,29 @@ class UserController < ApplicationController
       render 'new'
     end
   end
+
+	def edit
+    @user = User.find(params[:id])
+  end
+
+	def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+	def delete
+		@user=User.find(params[:id])
+	end
+
+	def destroy
+		@user=User.find(params[:id])
+		@user.destroy
+		redirect_to@user
+	end
 
   def user_params
     params.require(:books).permit(:name, :phone_number, :email, :username,:password, :location, :profile_picture)
